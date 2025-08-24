00f02d6caff93f48305ba0a17f05d93c57cd4530
aliaksei <ntkv2311@gmail.com>
2025-08-24 14:57:35 +0300

    fix232131

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '51715057-18e32c1eb7da364006690b614';
 
-export const PER_PAGE = 44;
+export const PER_PAGE = 15;
 
 export async function getImagesByQuery(query, page = 1) {
   const response = await axios.get(BASE_URL, {
